d1b9f2d8810f76449d0fa12e278c09a3fd7464db
Alren Estoque <alrenestoque1330@gmail.com>
2021-10-31 18:26:08 +0800

    Initial Setup

---
+++ android/app/src/main/kotlin/ph/com/bluehive/todo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package ph.com.bluehive.todo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyTodoApp());
+}
+
+class MyTodoApp extends StatelessWidget {
+  const MyTodoApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
